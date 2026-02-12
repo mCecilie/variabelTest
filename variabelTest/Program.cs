de584ffd1772761095e1a70f549bb560e718cfdb
Cecilie <mcmo79137@edu.ucl.dk>
2026-02-12 11:16:18 +0700

    Tilgøjer variabler til min terningekastsimulator

--- variabelTest/Program.cs
+++ variabelTest/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, You!");
+            Console.WriteLine("===MIN LOMMEREGNER===");
+
+            //Først bruger jeg Random klassen
+            Random terning = new Random();
+
+            //Herefter definerer jeg mine variable
+            int kast1 = terning.Next(1, 7); //Terningen kan vise mellem 1-6
+            int kast2 = terning.Next(1, 7); //Terningen kan vise mellem 1-6
+
+            int sum = kast1 + kast2;
         }
     }
 }
--- variabelTest/obj/Debug/net10.0/variabelTest.AssemblyInfo.cs
+++ variabelTest/obj/Debug/net10.0/variabelTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("variabelTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955b80f14f3ff60f2f731a9e9bd362d9f3133832")]
 [assembly: System.Reflection.AssemblyProductAttribute("variabelTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("variabelTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
